ccf3a1d91337b5768fa5bbd57d4749ac8d3bd0fb
hjkim <rukegg@gmail.com>
2022-11-01 18:00:27 +0900

    사칙연산 추가

---
+++ src/BJSTUDY221101_체스.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class BJSTUDY221101_체스 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int king = sc.nextInt();
+        int queen = sc.nextInt();
+        int rook = sc.nextInt();
+        int bishop = sc.nextInt();
+        int knight = sc.nextInt();
+        int pawn = sc.nextInt();
+
+        System.out.println((1 - king) + " " + (1 - queen) + " " + (2 - rook) + " " + (2 - bishop) + " " + (2 - knight) + " " + (8 - pawn));
+    }
+}
